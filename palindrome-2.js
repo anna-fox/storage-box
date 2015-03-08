8e625c9b572954aff2f9b2c833f2b7d828a16ac7
anna-fox <anna.owsienko@yandex.ru>
2015-03-09 00:23:21 +0300

    one more version of isPalindrome function

---
+++ palindrome-2.js
@@ -0,0 +1,19 @@
+console.log(isPalindrome('Казак'));
+console.log(isPalindrome('А роза упала на лапу Азора'));
+console.log(isPalindrome('Do geese see God?'));
+console.log(isPalindrome('Бряк'));
+
+function isPalindrome(s) {
+    var reg = /[a-zа-яё+]/gi;
+    var strArr = s.toLowerCase().match(reg),
+        arrLen = strArr.length,
+        comparisonSize = Math.floor(arrLen/2);
+
+    for (var i = 0; i < comparisonSize; i++){
+        if (strArr[i] !== strArr[arrLen - 1 - i]){
+            return false;
+        }
+    }
+
+    return true;
+}
