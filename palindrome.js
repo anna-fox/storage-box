fc66095d85e0a224ebf4d7864033b377f00ca8a8
anna-fox <anna.owsienko@yandex.ru>
2015-03-09 00:08:31 +0300

    isPalindrome  function

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+isPalindrome('Казак'); // true
+isPalindrome('А роза упала на лапу Азора'); // true
+isPalindrome('Do geese see God?'); // true
+isPalindrome('Бряк'); // false
+
+function isPalindrome(s) {
+    var reg = /[^a-zа-яё+]/gi;
+    var str = s.toLowerCase().replace(reg, ''),
+        revertedStr = str.split('').reverse().join('');
+    console.log(str === revertedStr);
+    return str === revertedStr;
+}
